4bf93b00f3a969f3f3d36b3eb994f233505b2d3a
Darko <darkopetrovic26@gmail.com>
2021-03-16 13:33:09 +0100

    Frame, small changes, ES6 verification

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
-/*
-  Made by: Darko Petrović
-
+/**
   ...No moon is there, no voice, no sound
   Of beating heart; a sigh profound...
 
-  FB: https://www.facebook.com/WitchkingOfAngmarr
-  CodeForces: https://codeforces.com/profile/darkoxv88
-  GitHub: https://github.com/darkoxv88
 
-  Version: 1.0.0
+	* @author Darko Petrovic
+  * @Link Facebook: https://www.facebook.com/WitchkingOfAngmarr
+  * @Link GitHub: https://github.com/darkoxv88
+  * @Link CodeForces: https://codeforces.com/profile/darkoxv88
 
-  Software can be modified, used commercially, and distributed.
-  Software can be modified and used in private.
-  A license and copyright notice must be included in the software.
-  Software authors provide no warranty with the software and are not liable for anything.
-*/
+	* @fileoverview main.js provides a demo of dp-utility.js and dp-3d-engine
+  * @source https://github.com/darkoxv88/dpUtility
+  * @version 1.0.0
 
 
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-class Engine extends dp3Dengine {
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+**/
+
+"use strict";
+
+dpVerifyES6();
+
+class Engine extends dp3dEngine {
 
   loadView(ele) {
     let view = dp.$('<app-display>')[0];
     view.innerHTML = '';
     view.append(ele);
-  }
-
-  onStart() {
-    
   }
 
 }
@@ -36,19 +50,19 @@ let index = {
   test : null,
   _3D : null,
 
-  init : function (buildEngine=false) {
+  init : function() {
     let c = new dpAJAX();
-
     c.get('test/test.html').then((data) => {
       console.log(data);
     });
 
+    dp.$('<app-display>')[0].innerHTML = `
+      <input style="margin: 6px;" type="file" onchange="index.test.loadImage(event)"></input>
+      <img id="org-img" style="margin: 6px;" width="400px" height="auto"></img>
+      <img id="test-img" style="margin: 6px;" width="400px" height="auto"></img>
+    `;
+
     this.test = new dpImageProcessing(async () => {
-      /*
-      	<input type="file" onchange="index.test.loadImage(event)"></input>
-        <img style="margin: 6px;" id="org-img" width="500px" height="auto"></img>
-			  <img id="test-img" width="500px" height="auto"></img>
-      */
       await this.test.asyncTemperature(6200);
       await this.test.asyncLightness(7);
       await this.test.asyncGamma(1.15)
@@ -62,14 +76,11 @@ let index = {
       dp.$("#test-img").src = this.test.getImg();
     });
 
-    if(buildEngine) {
+    if(false) {
       this._3D = new Engine(1);
     }
   },
 };
-
-dpComponents.register('app-a', '.a{display: block}', null, 'function', class test{});
-dpComponents.register('app-b', '.b{display: block}', null, 'function', class test{});
 
 dp.registerOnload(() => {
   index.init();
